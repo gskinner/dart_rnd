58c7a7af8d0f673be8b0266a45998236af17273e
Steve Alexander <steve@stevea.com>
2020-12-10 19:12:18 +0100

    add a test and convert to null safe

--- test/rnd_test.dart
+++ test/rnd_test.dart
@@ -1,6 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:rnd/rnd.dart';
+
 void main() {
-  test('test name', () {
+  final int seed = 42;
+  test('RndExtensions call() no arguments functions like nextDouble()', () {
+    expect(Random(seed)(), Random(seed).nextDouble());
+  });
+  test('RndExtensions call(max) functions like getDouble(0, max)', () {    
+    expect(Random(seed)(100.2), Random(seed).getDouble(0, 100.2));
+  });
+  test('RndExtensions call(min, max) functions like getDouble(min, max)', () {    
+    expect(Random(seed)(-100.2, 100.2), Random(seed).getDouble(-100.2, 100.2));
+  });
+  test('RndExtensions call(null, max) fails with an exception', () {    
+    expect(() => Random(seed)(null, 100.2), throwsArgumentError);
   });
 }
